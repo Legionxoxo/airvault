8887ca92380b61f855412712a6842a6459268d0e
LEGION <chandelshiv45@gmail.com>
2024-11-11 14:10:10 +0530

    fixed responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -19,11 +19,14 @@ export default function Home() {
                 <div className="absolute inset-x-0 mx-auto -mt-16 sm:-mt-16 md:-mt-16 lg:-mt-16 h-[300px] w-[300px] sm:h-[400px] sm:w-[400px] md:h-[500px] md:w-[600px] lg:h-[600px] lg:w-[800px] rounded-full bg-[#FF7733] opacity-[0.1] blur-[80px] mix-blend-lighten"></div>
                 <div className="absolute inset-x-0 mx-auto -mt-16 sm:-mt-16 md:-mt-16 lg:-mt-16 h-[300px] w-[300px] sm:h-[400px] sm:w-[400px] md:h-[500px] md:w-[600px] lg:h-[600px] lg:w-[800px] rounded-full bg-[#FF7733] opacity-[0.05] blur-[120px] mix-blend-lighten"></div>
 
-                {/* Layer for shadow.svg (below the text) */}
-                <div className="relative">
-                    <div className="absolute inset-0 w-full h-full bg-[url('/shadow.svg')] bg-cover bg-center opacity-75 mix-blend-overlay z-0"></div>
+                <div className="relative 2xl:h-screen">
+                    <img
+                        src="/shadow.png"
+                        alt="Shadow"
+                        className="absolute inset-0 w-full h-full object-cover opacity-75 mix-blend-overlay z-0"
+                    />
 
-                    <div className=" relative z-10 -mt-24">
+                    <div className="relative z-10 -mt-24 2xl:-mt-[100px]">
                         <WhoAirVault />
                     </div>
                 </div>
