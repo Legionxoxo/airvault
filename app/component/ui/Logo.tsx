9507568900ada588245b979599626562b3bf6bdc
LEGION <chandelshiv45@gmail.com>
2024-11-13 00:02:56 +0530

    link to page

--- app/component/ui/Logo.tsx
+++ app/component/ui/Logo.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Logo() {
     return (
         <div className="pt-4 sm:pt-6 ml-4 md:ml-[80px] lg:ml-[80px] xl:-ml-20">
-            <Image
-                src="/logo.svg"
-                alt="logo"
-                width={32}
-                height={32}
-                className=""
-            />
+            <Link href="/signup">
+                <Image
+                    src="/logo.svg"
+                    alt="logo"
+                    width={32}
+                    height={32}
+                    className=""
+                />
+            </Link>
         </div>
     );
 }
